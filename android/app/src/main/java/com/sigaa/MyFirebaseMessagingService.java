8d1178444fccdffbbf56c63fc7afd70b351203d2
Matheus William <matheuswr89@outlook.com>
2023-01-22 22:32:48 -0300

    feat: remove old folder structure

--- android/app/src/main/java/com/sigaa/MyFirebaseMessagingService.java
+++
@@ -1,6 +0,0 @@
-package com.sigaa;
-
-import com.google.firebase.messaging.FirebaseMessagingService;
-
-public class MyFirebaseMessagingService extends FirebaseMessagingService {
-}
--- index.js
+++
@@ -1,5 +0,0 @@
-import {AppRegistry} from 'react-native';
-import './shim.js';
-import App from './src/App';
-
-AppRegistry.registerComponent('sigaa', () => App);
